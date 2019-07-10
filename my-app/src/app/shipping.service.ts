70b637d46d12d151ab9530d82a9f3769eb39d46b
danelia1998 <davit.danelia.1@btu.edu.ge>
2019-07-10 20:02:12 +0400

    feat(20-di-part-3): Finish Tutorial

--- my-app/src/app/shipping.service.ts
+++ my-app/src/app/shipping.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class ShippingService {
+private type = 'None';
 
  constructor(
    private http: HttpClient
@@ -10,5 +13,13 @@ export class ShippingService {
 
  getShippingPrices() {
    return this.http.get('/assets/shipping.json');
+ }
+
+ getType(){
+   return this.type;
+ }
+
+ selectType(type) {
+   this.type = type;
  }
 }
